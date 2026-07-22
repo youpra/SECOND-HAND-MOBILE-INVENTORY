40a1dbfaf9df776f7062099f0d025d9e818595aa
Youm Patel <youmpatel@gmail.com>
2026-07-22 23:39:16 +0530

    fix: add missing serverFunction prop to RootLayout in payload layout.tsx

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import config from "@/payload.config";
 import "@payloadcms/next/css";
-import { RootLayout } from "@payloadcms/next/layouts";
+import { handleServerFunctions, RootLayout } from "@payloadcms/next/layouts";
 import { importMap } from "./admin/importMap.js";
+import type { ServerFunctionClient } from "payload";
+
+const serverFunction: ServerFunctionClient = async function (args) {
+  "use server";
+  return handleServerFunctions({ ...args, config, importMap });
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <RootLayout importMap={importMap} config={config}>
+    <RootLayout
+      importMap={importMap}
+      config={config}
+      serverFunction={serverFunction}
+    >
       {children}
     </RootLayout>
   );
